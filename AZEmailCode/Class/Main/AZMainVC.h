7bc0305fd9544bdd62331c51cfe7513d4b8dad0c
徐振 <xuzhen@xuzhendeMacBook-Pro.local>
2017-03-27 10:16:11 +0800

    创建目录

---
+++ AZEmailCode/Class/Main/AZMainVC.h
@@ -0,0 +1,13 @@
+//
+//  AZMainVC.h
+//  AZEmailCode
+//
+//  Created by 徐振 on 2017/3/27.
+//  Copyright © 2017年 徐振. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AZMainVC : UIViewController
+
+@end
---
+++ AZEmailCode/Class/Main/AZMainVC.m
@@ -0,0 +1,37 @@
+//
+//  AZMainVC.m
+//  AZEmailCode
+//
+//  Created by 徐振 on 2017/3/27.
+//  Copyright © 2017年 徐振. All rights reserved.
+//
+
+#import "AZMainVC.h"
+
+@interface AZMainVC ()
+
+@end
+
+@implementation AZMainVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
